e825e4c82c022ad842687b801987f01d0c5094e8
Dmitry <dimademkin3010@gmail.com>
2023-07-31 16:27:12 +0500

    Приложение + пофиксил баги

--- app/src/main/java/com/example/giftchanger/ViewsActivity.java
+++ app/src/main/java/com/example/giftchanger/ViewsActivity.java
@@ -28,7 +28,7 @@ public class ViewsActivity extends AppCompatActivity implements View.OnClickList
     private static final List<Integer> AGE_0_2 = Arrays.asList(0, 1, 2);
     private static final List<Integer> AGE_3_6 = Arrays.asList(3,4,5,6);
     private static final List<Integer> AGE_7_12 = Arrays.asList(7,8,9,10,11,12);
-    private static final List<Integer> AGE_13_17 = Arrays.asList(13_17);
+    private static final List<Integer> AGE_13_17 = Arrays.asList(13, 14, 15, 16, 17);
     private static final List<Integer> AGE_35_60 = Arrays.asList(36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60);
     private static final List<Integer> AGE_61_100 = Arrays.asList(61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100);
     protected void onCreate(Bundle savedInstanceState) {
